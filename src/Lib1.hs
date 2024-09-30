20b7291d8bf90b4bd16d98e587cea5191f1ab15a
aleksandr.sventickij <aleksandr.sventickij@gmail.com>
2024-09-30 18:33:02 +0000

    Lab1

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -5,4 +5,4 @@ module Lib1
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions = ["car_garage","car_list","car","brand","model","year","color","operations","add_car","remove_car","get_list"]
